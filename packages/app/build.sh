cd3772ff90075b1ab35baa7c7919d4ff2b41182e
Andrew Henry <andrew.henry@pitt.edu>
2022-01-13 01:14:13 -0500

    feat: Add tags for releases

--- packages/app/build.sh
+++ packages/app/build.sh
@@ -2,4 +2,6 @@ sha=$(git log --pretty=format:'%h' -n 1)
 echo "export const hotPatchVersion = '"$sha"'" >./generated/constants.ts
 git add *generated/constants.ts
 git commit -m "ci: Bumped sha version to '"$sha"'"
+git tag "$sha"
 git push
+git push --tags
