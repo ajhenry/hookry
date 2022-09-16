3dedc01f0bdf953c6a070f4645a5f8b357d15aab
Andrew Henry <andrew.henry@pitt.edu>
2022-09-16 19:46:57 -0400

    fix

--- packages/app/app.config.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  return {
-    name: process.env.APP_ENV === "production" ? "Hookry" : "Hookry DEV",
-    ios: {
-      bundleIdentifier:
-      process.env.APP_ENV === "production" ? "com.ajhenry.hookry" : "com.ajhenry.hookrydev",
-    },
-  };
-};
